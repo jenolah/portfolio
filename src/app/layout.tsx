570d32103c51faf883714af729aa61aed202d0ac
jenolah <olahjendev@gmail.com>
2023-11-11 14:30:18 +0100

    remove unused import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Footer } from "@/app/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
