bd1ee15148944a7be6dd690edc0334735a86a2f5
jenolah <olahjendev@gmail.com>
2023-12-01 22:35:07 +0100

    change cloud color

--- src/app/components/Clouds.tsx
+++ src/app/components/Clouds.tsx
@@ -5,17 +5,17 @@ export const Clouds = () => {
         className="absolute animate-clouds"
         style={{ zIndex: -1, top: `40vh`, left: `90vw` }}
       >
-        <div className="absolute top-2 left-2 p-9 rounded-full bg-slate-200" />
-        <div className="absolute top-9 px-16 py-9 rounded-full bg-slate-200" />
-        <div className="absolute left-12 px-8 py-8 rounded-full bg-slate-200" />
+        <div className="absolute top-2 left-2 p-9 rounded-full bg-slate-100" />
+        <div className="absolute top-9 px-16 py-9 rounded-full bg-slate-100" />
+        <div className="absolute left-12 px-8 py-8 rounded-full bg-slate-100" />
       </div>
       <div
         className="absolute animate-clouds"
         style={{ zIndex: -1, top: `10vh`, left: `20vw` }}
       >
-        <div className="absolute top-2 left-2 p-9 rounded-full bg-slate-200" />
-        <div className="absolute top-9 px-16 py-9 rounded-full bg-slate-200" />
-        <div className="absolute left-12 px-8 py-8 rounded-full bg-slate-200" />
+        <div className="absolute top-2 left-2 p-9 rounded-full bg-slate-100" />
+        <div className="absolute top-9 px-16 py-9 rounded-full bg-slate-100" />
+        <div className="absolute left-12 px-8 py-8 rounded-full bg-slate-100" />
       </div>
     </div>
   );
